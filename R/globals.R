26857dfddab9c566ef26f186d9b1316c3f708514
jinhaslab <jinhaslab@gmail.com>
2026-03-18 10:47:04 +0900

    Fix R CMD check warnings: license, globals, buildignore

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+# Define global variables for tidyverse functions to prevent R CMD check warnings
+utils::globalVariables(c(
+  ".", "avg", "fit", "ll", "n", "name", "or", "OR95CI", "order", "p.value", 
+  "prob", "ranks", "smry", "std", "tidied", "ul", "values", "variables", "Variables"
+))
